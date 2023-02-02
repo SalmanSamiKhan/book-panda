58208933642270dfa8a558e0b070661998ba71d2
Salman <salmansamiikhan@gmail.com>
2023-02-03 04:59:47 +0600

    complete homepage design

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import Navbar from "./components/Navbar";
+import Books from "./components/Books";
+function App() {
+  return (
+    <div>
+      <header>
+        <Navbar/>
+      </header>
+      <main>
+          <Books/>
+      </main>
+      <footer>
+
+      </footer>
+    </div>
+  );
+}
+
+export default App;
